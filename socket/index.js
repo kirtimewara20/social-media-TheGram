9da8a48848da4ced29a9aa28862bec49d8630277
kirtimewara20 <mewarakirti20@gmail.com>
2024-01-30 00:43:48 +0530

    third commit

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(process.env.PORT || 8800,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:["http://localhost:3000", "https://frontend-27lh.onrender.com"]
     },
 });
 
